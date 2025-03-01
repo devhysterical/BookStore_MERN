85b6991a15a0f9da9bd46a1545b1bb5a6ea1a71c
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-03-01 18:19:39 +0700

    Add Navbar component with user dropdown and search functionality; include avatar image and react-icons dependencies

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,103 @@
-import React from 'react'
+// import React from 'react'
+import { Link } from "react-router-dom";
+import { HiBars3 } from "react-icons/hi2";
+import { HiOutlineSearch } from "react-icons/hi";
+import { HiOutlineUserCircle } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi2";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+
+import avatarImg from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Orders", href: "/orders" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Check Out", href: "/checkout" },
+];
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // console.log(isDropdownOpen);
+  const currentUser = true;
   return (
-    <div>
-      This is Navbar
-    </div>
-  )
-}
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
+      <nav className="flex justify-between items-center">
+        {/* Left side */}
+        <div className="flex items-center md:gap-16 gap-8">
+          <Link to="/">
+            <HiBars3 className="size-6" />
+          </Link>
+
+          {/* Search */}
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <HiOutlineSearch className="absolute inline-block left-3 inset-y-2" />
+
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="relative flex items-center md:space-x-2">
+          <div>
+            {currentUser ? (
+              <>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  <img
+                    src={avatarImg}
+                    alt=""
+                    className={`size-7 rounded-full ${
+                      currentUser ? "ring-2 ring-blue-500" : ""
+                    }`}
+                  />
+                </button>
+
+                {/* Show dropdown */}
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="">
+                      {navigation.map((item) => (
+                        <li
+                          key={item.name}
+                          onClick={() => setIsDropdownOpen(false)}>
+                          <Link
+                            to={item.href}
+                            className="block px-4 py-2 text-sm hover:bg-gray-100">
+                            {item.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to="/login">
+                <HiOutlineUserCircle className="size-6" />
+              </Link>
+            )}
+          </div>
+
+          {/* Heart button */}
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          {/* Cart icon */}
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 py-2 flex items-center rounded-sm">
+            <HiOutlineShoppingCart className="size-6" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
