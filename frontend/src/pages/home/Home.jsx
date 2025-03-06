1aeedc731965cb2b52e08e6c055dcc3b26c698a3
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-03-06 09:40:30 +0700

    Add utility functions for base and image URLs, implement TopSellers component with category filtering and Swiper integration, and create BooksCard component for displaying book details

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import Banner from './Banner'
+import TopSellers from './TopSellers'
 
 export default function Home() {
   return (
     <>
-    <Banner />
+      <Banner />
+      <TopSellers />
     </>
   )
 }
