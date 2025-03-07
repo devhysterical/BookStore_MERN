b668f2261561b674351d3d1e7d873065219f41cc
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-03-07 22:10:51 +0700

    Add Recommend component to Home page for displaying recommended books with Swiper integration

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
-import Banner from './Banner'
-import TopSellers from './TopSellers'
+import Banner from "./Banner";
+import Recommend from "./Recommend";
+import TopSellers from "./TopSellers";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <TopSellers />
+      <Recommend />
     </>
-  )
+  );
 }
