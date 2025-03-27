267f61bb11bf2b5710fc045258a83b2b4dd70105
Dev.Hysterical <2124802010307@student.tdmu.edu.vn>
2025-03-27 10:57:49 +0700

    fix(cors): update allowed origin for CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // Báo lỗi thì đổi port, từ 3000 den 5000
 require("dotenv").config();
 
 // middleware
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5174"],
     credentials: true,
     // methods: "GET,POST,PUT,DELETE",
     // allowedHeaders: "Content-Type,Authorization",
